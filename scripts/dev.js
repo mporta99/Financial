a575279b2854093d6254f921e25c7b75023b32ae
mporta99 <m.v.m.p99@gmail.com>
2026-04-04 09:50:26 -0300

    MPORTA - SQLITE RUNNING - V02

---
+++ scripts/dev.js
@@ -0,0 +1,68 @@
+const { spawn } = require("child_process");
+
+const isWindows = process.platform === "win32";
+const children = [];
+
+function startProcess(name, command, color) {
+  const child = spawn(command, {
+    cwd: process.cwd(),
+    env: process.env,
+    shell: isWindows,
+    stdio: ["inherit", "pipe", "pipe"]
+  });
+
+  const prefix = `\x1b[${color}m[${name}]\x1b[0m`;
+
+  child.stdout.on("data", (chunk) => {
+    process.stdout.write(`${prefix} ${chunk}`);
+  });
+
+  child.stderr.on("data", (chunk) => {
+    process.stderr.write(`${prefix} ${chunk}`);
+  });
+
+  child.on("exit", (code) => {
+    const message = `${prefix} exited with code ${code}\n`;
+    if (!shuttingDown) {
+      process.stderr.write(message);
+      shutdown(code ?? 0);
+      return;
+    }
+
+    process.stdout.write(message);
+  });
+
+  children.push(child);
+  return child;
+}
+
+let shuttingDown = false;
+
+function shutdown(exitCode) {
+  if (shuttingDown) {
+    return;
+  }
+
+  shuttingDown = true;
+
+  for (const child of children) {
+    if (!child.killed) {
+      child.kill("SIGINT");
+    }
+  }
+
+  setTimeout(() => {
+    for (const child of children) {
+      if (!child.killed) {
+        child.kill("SIGTERM");
+      }
+    }
+    process.exit(exitCode);
+  }, 500);
+}
+
+process.on("SIGINT", () => shutdown(0));
+process.on("SIGTERM", () => shutdown(0));
+
+startProcess("server", "npm run server:dev", "36");
+startProcess("client", "npm run client:dev", "35");
